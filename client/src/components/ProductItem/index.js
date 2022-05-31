d7079acf672dd7d42f22c64bd516bb3dd595f3e3
unknown <vulong.pham26@gmail.com>
2022-05-31 18:56:08 -0400

    fixed SASS and added some styling.

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -4,6 +4,8 @@ import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import '../../styles/ProductItem.scss';
+
 
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
